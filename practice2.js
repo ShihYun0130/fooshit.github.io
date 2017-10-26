317ff6a0b879ce1352b0d9e4a76114b410cf6121
fooshit <33059448+fooshit@users.noreply.github.com>
2017-10-26 16:47:08 +0800

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,18 @@
+function multiTable(){
+	//document.getElementById("Table").="HELLO";
+	var table = document.getElementById("Table");
+	var i = 1;
+	var j = 1;
+	while(i <= 9)
+	{
+		while(j <= 9)
+		{	
+			table.innerHTML += "<div>"+i*j+"</div>";
+			table.innerHTML += " ";
+			j++;
+		}
+		j = 1;
+		i++;
+		table.innerHTML += "<br>";
+	}
+}
