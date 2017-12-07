3774439fb6a18c13d9f4b04ae62268f5e694467e
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-07 19:23:39 +0800

    Update practice2.1(a).js

--- practice2.1(a).js
+++ practice2.1(a).js
@@ -1,6 +1,6 @@
 function findRound(){
 	var input = document.getElementById("enterAbs").value;
-	output = Math.round(Math.pow(input,2));
+	var output = Math.round(Math.pow(input,2));
 	var absResult = document.getElementById("absNumber");
 	absResult.innerHTML = output;
 }
