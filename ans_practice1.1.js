3bb1c40d832e509946533246d0eacc16f566821b
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-07 20:36:29 +0800

    Add files via upload

--- ans_practice1.1.js
+++ ans_practice1.1.js
@@ -2,22 +2,17 @@ function countBMI() {
     var weight = document.getElementById("enterWeight").value;
     var height = document.getElementById("enterHeight").value;
     //----------practice1.1-------------------------
-    //宣告兩個變數，分別把他們的值變成為"weight"、"height"再宣告一個變數用來存取計算後的BMI值吧!
-    var;
-    var;
-    var;
+    var x = weight;
+    var y = height;
+    var bmi;
 
     //----------practice1.2-------------------------
-    //寫出BMI的計算公式吧!
-    bmi = ;
+    bmi = x / ((y / 100) * (y / 100));
     //----------practice1.3-------------------------
-    /*這裡應該填上什麼條件式呢?*/
-    {
+    if (bmi >= 22) {
         var resultBMI = document.getElementById("BMINumber");
         resultBMI.innerHTML = "your BMI is:  " + bmi + ",and you have to go on a diet :((";
-    }
-    /*這裡應該填上什麼條件式呢?*/
-    {
+    } else {
         var resultBMI = document.getElementById("BMINumber");
         resultBMI.innerHTML = "your BMI is:  " + bmi + ",and your BMI is just fine :))";
     }
