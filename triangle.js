7cca2a9c16f79c9f3b6017ad02fc878facefc9f7
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-07 20:44:22 +0800

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,10 +1,13 @@
 
 function checkTriangle(){
 	var answer = document.getElementById("triangleResult");
-        var edge1 = Number(document.getElementById("edge1").value);
-	var edge2 = Number(document.getElementById("edge2").value);
-	var edge3 = Number(document.getElementById("edge3").value);
+        var edge1 = document.getElementById("edge1").value;
+	var edge2 = document.getElementById("edge2").value;
+	var edge3 = document.getElementById("edge3").value;
 	var x ;
+	edge1 = Number(edge1);
+	edge2 = Number(edge2);
+	edge3 = Number(edge3);
 	
 	if (edge1 + edge2 > edge3 && edge2 + edge3 > edge1 && edge1 + edge3 > edge2) {
 		if (pow(edge1,2) == pow(edge2,2) + pow(edge3,2) || pow(edge2,2) == pow(edge1,2) + pow(edge3,2) ||pow(edge3,2) == pow(edge2,2) + pow(edge1,2)) {
