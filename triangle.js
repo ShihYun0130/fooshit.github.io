e59bd3587a921117e6c106ae7fb2dd67cbde5f7c
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-07 20:33:25 +0800

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -6,7 +6,7 @@ function checkTriangle(){
 	var edge3 = document.getElementById("edge3").value;
 	var x ;
 	
-	if ((edge1 + edge2 > edge3) && (edge2 + edge3 > edge1) && (edge1 + edge3 > edge2)) {
+	if (edge1 + edge2 > edge3 && edge2 + edge3 > edge1 && edge1 + edge3 > edge2) {
 
 		if (pow(edge1,2) == pow(edge2,2) + pow(edge3,2) || pow(edge2,2) == pow(edge1,2) + pow(edge3,2) ||pow(edge3,2) == pow(edge2,2) + pow(edge1,2)) {
 				x = "是，直角三角形";
