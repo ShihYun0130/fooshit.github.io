7449a89fb46e16bd9880b848675e9333b036718e
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-07 20:22:17 +0800

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -4,15 +4,16 @@ function checkTriangle(){
         var edge1 = document.getElementById("edge1").value;
 	var edge2 = document.getElementById("edge2").value;
 	var edge3 = document.getElementById("edge3").value;
+	var x ;
 	
 	if (edge1 + edge2 > edge3 && edge2 + edge3 > edge1 && edge1 + edge3 > edge2) {
 
 		if (pow(edge1,2) == pow(edge2,2) + pow(edge3,2) || pow(edge2,2) == pow(edge1,2) + pow(edge3,2) ||pow(edge3,2) == pow(edge2,2) + pow(edge1,2)) {
-				var x = "是，直角三角形";
+				x = "yes";
 		}
 	}
 	else{
-		var x = "否";
+		x = "no";
 	}
 	answer.innerHTML = x;
 }
