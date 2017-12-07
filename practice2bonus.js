60f9b1f47448d522a813dd9f92a4ed7adfa2bb21
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-07 19:26:27 +0800

    Update practice2bonus.js

--- practice2bonus.js
+++ practice2bonus.js
@@ -1,7 +1,8 @@
 function findRandom(){
 	var ups = document.getElementById("enterUps").value;
-
-	var x = Math.floor (Math.random() * ups );
+	
+	var output = Math.floor (Math.random() * ups );
+	
 	var randomResult = document.getElementById("randomNumber");
-	randomResult.innerHTML = x;
+	randomResult.innerHTML = output;
 }
