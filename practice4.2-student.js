532af06614349f932d471b435c71b800efeaa104
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-22 15:43:08 +0800

    Add files via upload

---
+++ practice4.2-student.js
@@ -0,0 +1,19 @@
+function generateTriangle()
+{
+	var y = document.getElementById("demon");
+	y.innerHTML = "";
+
+
+
+	/*-----宣告你的for迴圈-----*/
+	
+	{
+		/*-----再次宣告for迴圈-----*/
+	    
+	    {	
+	    	/*-----寫出你想要印出的符號吧！-----*/
+	        y.innerHTML += "";
+	    }
+	    y.innerHTML += "<br>";
+	}
+}
