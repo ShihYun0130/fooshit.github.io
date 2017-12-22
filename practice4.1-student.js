5686ea86bfbf0ae42e9f079212364dbef1497141
fooshit <33059448+fooshit@users.noreply.github.com>
2017-12-22 15:42:16 +0800

    Add files via upload

---
+++ practice4.1-student.js
@@ -0,0 +1,24 @@
+function checkAns(y)
+{
+	var x = document.getElementById("one").value;
+	x = Number(x);
+	
+
+	/*-----提醒：電腦設定的隨機數字叫做y喔-----*/
+	/*-----提醒：你所猜的數字叫作x喔-----*/
+
+	/*-----比較你輸入的數跟答案誰大誰小吧！-----*/
+	
+	{
+		/*-----底下的" "裡面要輸入提示的字喔，例如"答對了！"這樣-----*/
+	    document.getElementById("printOut").innerHTML = "";
+	} 
+	
+	{
+	    document.getElementById("printOut").innerHTML = "";
+	} 
+	
+	{
+	    document.getElementById("printOut").innerHTML = "";
+	}
+}
